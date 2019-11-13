ea2afcca2066e3e489bfd62095e85183b305c15d
Warsame Yusuf <wyusu068@Uottawa.ca>
2019-11-13 14:18:29 -0500

    [Feature] Documented datasets for rest of CCHS cycles on data.R. Renamed DDI files

--- R/data.R
+++ R/data.R
@@ -1,12 +1,34 @@
-#' 2003 CCHS data
+#' 2001 CCHS data
 #' 
-#' This is a subset of 200 observations from the 2003 cycle of the Canadian Community Health Survey (CCHS) 
+#' This is a subset of 200 observations from the 2001 cycle of the Canadian Community Health Survey (CCHS) 
 #' Public Use Microdata file (PUMF) dataset.This survey is conducted by Statistics Canada. The CCHS 
 #' has a [Statistics Canada Open License](https://www.statcan.gc.ca/eng/reference/licence).
 #' 
+#' Long name: cchs-82M0013-E-2001-c1-1-general-file
+#' 
+#' DDI: '~/inst/extdata/CCHS_DDI/CCHS_2001_DDI.xml
+#' 
+#' @name cchs2001
+#' @aliases cchs2001
+#' @docType data
+#' @return \item{cchs2001}{a data frame}
+#' 
+#' @source http://www23.statcan.gc.ca/imdb/p2SV.pl?Function=getSurvey&Id=3359
+#' 
+#' @keywords datasets
+#' @examples 
+#' data(cchs2001)
+#' str(cchs2001)
+NULL
+
+#' 2003 CCHS data
+#' 
+#' This is a subset of 200 observations from the 2003 cycle of the CCHS PUMF dataset.
+#' 
 #' Long name: cchs-82M0013-E-2003-c2-1-General File
 #' 
-#' DDI: '~/inst/extdata/CCHS_DDI/cchs-82M0013-E-2003-c2-1-GeneralFile.xml
+#' DDI: '~/inst/extdata/CCHS_DDI/CCHS_2003_DDI.xml
+#' 
 #' @name cchs2003
 #' @aliases cchs2003
 #' @docType data
@@ -20,13 +42,76 @@
 #' str(cchs2003)
 NULL
 
+#' 2005 CCHS data
+#' 
+#' This is a subset of 200 observations from the 2005 cycle of the CCHS PUMF dataset.
+#' 
+#' Long name: cchs-82M0013-E-2005-c3-1-main-file
+#' 
+#' DDI: '~/inst/extdata/CCHS_DDI/CCHS_2005_DDI.xml
+#' 
+#' @name cchs2005
+#' @aliases cchs2005
+#' @docType data
+#' @return \item{cchs2005}{a data frame}
+#' 
+#' @source http://www23.statcan.gc.ca/imdb/p2SV.pl?Function=getSurvey&Id=22642
+#' 
+#' @keywords datasets
+#' @examples 
+#' data(cchs2005)
+#' str(cchs2005)
+NULL
+
+#' 2007-2008 CCHS data
+#' 
+#' This is a subset of 200 observations from the 2007-2008 cycle of the CCHS PUMF dataset.
+#' 
+#' Long name: cchs-E-2007-2008-AnnualComponent
+#' 
+#' DDI: '~/inst/extdata/CCHS_DDI/CCHS_2007_2008_DDI.xml
+#' 
+#' @name cchs2007_2008
+#' @aliases cchs2007_2008
+#' @docType data
+#' @return \item{cchs2007_2008}{a data frame}
+#' 
+#' @source http://www23.statcan.gc.ca/imdb/p2SV.pl?Function=getSurvey&Id=29539
+#' 
+#' @keywords datasets
+#' @examples 
+#' data(cchs2007_2008)
+#' str(cchs2007_2008)
+NULL
+
+#' 2009-2010 CCHS data
+#' 
+#' This is a subset of 200 observations from the 2009-2010 cycle of the CCHS PUMF dataset.
+#' 
+#' Long name: CCHS-82M0013-E-2009-2010-Annualcomponent
+#' 
+#' DDI: '~/inst/extdata/CCHS_DDI/CCHS_2009_2010_DDI.xml
+#' 
+#' @name cchs2009_2010
+#' @aliases cchs2009_2010
+#' @docType data
+#' @return \item{cchs2009_2010}{a data frame}
+#' 
+#' @source http://www23.statcan.gc.ca/imdb/p2SV.pl?Function=getSurvey&Id=67251
+#' 
+#' @keywords datasets
+#' @examples 
+#' data(cchs2009_2010)
+#' str(cchs2009_2010)
+NULL
+
 #' 2010 CCHS data
 #' 
 #' This is a subset of 200 observations from the 2010 cycle of the CCHS PUMF dataset. 
 #' 
 #' Long name: CCHS-82M0013-E-2010-AnnualComponent
 #' 
-#' DDI: '~/inst/extdata/CCHS_DDI/CCHS-82M0013-E-2010-AnnualComponent.xml
+#' DDI: '~/inst/extdata/CCHS_DDI/CCHS_2010_DDI.xml
 #'
 #' @name cchs2010
 #' @aliases cchs2010
@@ -39,4 +124,88 @@ NULL
 #' @examples 
 #' data(cchs2010)
 #' str(cchs2010)
+NULL
+
+#' 2011-2012 CCHS data
+#' 
+#' This is a subset of 200 observations from the 2011-2012 cycle of the CCHS PUMF dataset. 
+#' 
+#' Long name: cchs-82M0013-E-2011-2012-Annual-component
+#' 
+#' DDI: '~/inst/extdata/CCHS_DDI/CCHS_2011_2012_DDI.xml
+#'
+#' @name cchs2011_2012
+#' @aliases cchs2011_2012
+#' @docType data
+#' @return \item{cchs2011_2012}{a data frame}
+#' 
+#' @source http://www23.statcan.gc.ca/imdb/p2SV.pl?Function=getSurvey&Id=114112
+#' 
+#' @keywords datasets
+#' @examples 
+#' data(cchs2011_2012)
+#' str(cchs2011_2012)
+NULL
+
+#' 2012 CCHS data
+#' 
+#' This is a subset of 200 observations from the 2012 cycle of the CCHS PUMF dataset. 
+#' 
+#' Long name: cchs-82M0013-E-2012-Annual-component
+#' 
+#' DDI: '~/inst/extdata/CCHS_DDI/CCHS_2012_DDI.xml
+#'
+#' @name cchs2012
+#' @aliases cchs2012
+#' @docType data
+#' @return \item{cchs2012}{a data frame}
+#' 
+#' @source http://www23.statcan.gc.ca/imdb/p2SV.pl?Function=getSurvey&Id=135927
+#' 
+#' @keywords datasets
+#' @examples 
+#' data(cchs2012)
+#' str(cchs2012)
+NULL
+
+#' 2013-2014 CCHS data
+#' 
+#' This is a subset of 200 observations from the 2013-2014 cycle of the CCHS PUMF dataset. 
+#' 
+#' Long name: cchs-82M0013-E-2013-2014-Annual-component
+#' 
+#' DDI: '~/inst/extdata/CCHS_DDI/CCHS_2013_2014_DDI.xml
+#'
+#' @name cchs2013_2014
+#' @aliases cchs2013_2014
+#' @docType data
+#' @return \item{cchs2013_2014}{a data frame}
+#' 
+#' @source http://www23.statcan.gc.ca/imdb/p2SV.pl?Function=getSurvey&Id=144170
+#' 
+#' @keywords datasets
+#' @examples 
+#' data(cchs2013_2014)
+#' str(cchs2013_2014)
+NULL
+
+#' 2014 CCHS data
+#' 
+#' This is a subset of 200 observations from the 2014 cycle of the CCHS PUMF dataset. 
+#' 
+#' Long name: cchs-82M0013-E-2014-Annual-component
+#' 
+#' DDI: '~/inst/extdata/CCHS_DDI/CCHS_2014_DDI.xml
+#'
+#' @name cchs2014
+#' @aliases cchs2014
+#' @docType data
+#' @return \item{cchs2014}{a data frame}
+#' 
+#' @source http://www23.statcan.gc.ca/imdb/p2SV.pl?Function=getSurvey&Id=164081
+#' 
+#' @keywords datasets
+#' @examples 
+#' data(cchs2014)
+#' str(cchs2014)
 NULL
