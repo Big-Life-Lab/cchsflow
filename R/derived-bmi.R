564a6e3136a8fbf39a8950ebaac14d69a1c25f23
Warsame Yusuf <wyusu068@Uottawa.ca>
2019-11-13 12:10:20 -0500

    [feature] added example of using function with user inputted data for ht/wt

--- R/derived-bmi.R
+++ R/derived-bmi.R
@@ -56,6 +56,14 @@ ifelse2 <- function(x, a, b) {
 #' 
 #' combined_bmi <- bind_rows(bmi2010, bmi2012)
 #' 
+#' @examples 
+#' # Using BMI_derived to generate a BMI value with user inputted height and weight values
+#' BMI_derived can also generate a value for BMI if you input a value for height and weight. Let's your height is
+#' 170cm (1.7m) and your weight is 50kg, your BMI can be calculated as followed:
+#' 
+#' library(cchsflow)
+#' BMI <- BMI_derived(1.7, 50)
+#' 
 #' @export
 BMI_derived <- 
   function(HWTGHTM, 
