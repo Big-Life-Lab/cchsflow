0a6b6fd2761db3b52cab7dab76b0cc960fe98845
Warsame Yusuf <wyusu068@Uottawa.ca>
2019-11-13 12:32:19 -0500

    [refactor] adding comment lines in .R file to properly generate .Rd file for BMI_derived()

--- R/derived-bmi.R
+++ R/derived-bmi.R
@@ -42,11 +42,11 @@ ifelse2 <- function(x, a, b) {
 #' 
 #' @examples 
 #' # Using BMI_derived to create BMI values across cycles
-#' BMI_derived is specified in variableDetails.csv along with the CCHS variables and cycles included. 
+#' # BMI_derived is specified in variableDetails.csv along with the CCHS variables and cycles included. 
 #' 
-#' To transform the derived BMI variable, use RecWTable() for each CCHS cycle and specify HWTGBMI_derived, 
-#' along with height (HWTGHTM) and weight (HWTGWTK). Then by using bind_rows(), you can combined derived BMI variables
-#' across cycles
+#' # To transform the derived BMI variable, use RecWTable() for each CCHS cycle and specify HWTGBMI_derived, 
+#' # along with height (HWTGHTM) and weight (HWTGWTK). Then by using bind_rows(), you can combined derived BMI variables
+#' # across cycles
 #' 
 #' bmi2010 <- RecWTable(dataSource = cchs2010, variableDetails = variableDetails, datasetName = "cchs2010", 
 #' variables = c("HWTGHTM", "HWTGWTK", "BMI_derived"))
@@ -56,10 +56,9 @@ ifelse2 <- function(x, a, b) {
 #' 
 #' combined_bmi <- bind_rows(bmi2010, bmi2012)
 #' 
-#' @examples 
 #' # Using BMI_derived to generate a BMI value with user inputted height and weight values
-#' BMI_derived can also generate a value for BMI if you input a value for height and weight. Let's your height is
-#' 170cm (1.7m) and your weight is 50kg, your BMI can be calculated as followed:
+#' # BMI_derived can also generate a value for BMI if you input a value for height and weight. Let's your height is
+#' # 170cm (1.7m) and your weight is 50kg, your BMI can be calculated as followed:
 #' 
 #' library(cchsflow)
 #' BMI <- BMI_derived(1.7, 50)
