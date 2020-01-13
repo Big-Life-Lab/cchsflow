620faba086d2d35f6dc0410122a927f6602ef6cb
DarkShadeKnigh <rostikisawesome@gmail.com>
2020-01-13 10:19:23 -0500

    [Refactor] Testing test fixes

--- tests/testthat/test-recode-with-table.R
+++ tests/testthat/test-recode-with-table.R
@@ -1,28 +1,34 @@
 load(file.path("..", "testdata", "rec_with_table_test_data.RData"))
 test_that("rec_with_table matches the standard 2001 recode", {
-  expect_equal(suppressWarnings(
-    rec_with_table(cchs2001,
-                   print_note = FALSE)),
+  out <- suppressWarnings(
+    cchsflow::rec_with_table(cchs2001,
+                             print_note = FALSE))
+  print(out[1:88,c(1,2,3,4,5,6,7,8,9)])
+  print(cchs2001Standard[1:88,c(1,2,3,4,5,6,7,8,9)])
+  expect_equivalent(out,
     cchs2001Standard)
 })
 
 test_that("rec_with_table matches the standard 2003 recode", {
-  expect_equal(suppressWarnings(
-    rec_with_table(cchs2003,
-                   print_note = FALSE)),
-    cchs2003Standard)
+  out <- suppressWarnings(
+    cchsflow::rec_with_table(cchs2003,
+                             print_note = FALSE))
+  expect_equivalent(out,
+                    cchs2003Standard)
 })
 
 test_that("rec_with_table matches the standard 2005 recode", {
-  expect_equal(suppressWarnings(
-    rec_with_table(cchs2005,
-                   print_note = FALSE)),
-    cchs2005Standard)
+  out <- suppressWarnings(
+    cchsflow::rec_with_table(cchs2005,
+                             print_note = FALSE))
+  expect_equivalent(out,
+                    cchs2005Standard)
 })
 
-test_that("rec_with_table matches the standard 20014 recode", {
-  expect_equal(suppressWarnings(
-    rec_with_table(cchs2014,
-                   print_note = FALSE)),
-    cchs2014Standard)
+test_that("rec_with_table matches the standard 2014 recode", {
+  out <- suppressWarnings(
+    cchsflow::rec_with_table(cchs2014,
+                             print_note = FALSE))
+  expect_equivalent(out,
+                    cchs2014Standard)
 })
