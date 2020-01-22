98cef9f45db4499ec397dd802c604b3bcaced168
Warsame Yusuf <wyusu068@Uottawa.ca>
2020-01-22 08:51:05 -0500

    [Refactor] minor typo fix

--- tests/testthat/test-food-insecurity.R
+++ tests/testthat/test-food-insecurity.R
@@ -17,7 +17,7 @@ test_that("food_insecurity_fun2 has expected output when FSCDHFS is out of
             expect_equal(food_insecurity_fun2(4), NA)
             })
 
-test_that("food_insecurity_fun1 has expected output when FSCDHFS is in
+test_that("food_insecurity_fun2 has expected output when FSCDHFS is in
           range", {
             expect_equal(food_insecurity_fun2(1), 2)
             })
@@ -29,7 +29,7 @@ test_that("food_insecurity_fun3 has expected output when FSCDHFS2 is out of
             expect_equal(food_insecurity_fun3(4), NA)
             })
 
-test_that("food_insecurity_fun1 has expected output when FSCDHFS2 is in
+test_that("food_insecurity_fun3 has expected output when FSCDHFS2 is in
           range", {
             expect_equal(food_insecurity_fun3(1), 2)
             })
