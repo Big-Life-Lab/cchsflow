123abc89caae4e3db58474419fee3e375726b624
Warsame Yusuf <wyusu068@Uottawa.ca>
2020-02-06 10:28:34 -0500

    [Refactor] reassign variables after handling NAs

--- R/multiple-conditions.R
+++ R/multiple-conditions.R
@@ -11,13 +11,13 @@ multiple_conditions_fun <-
     CCC_051 <- as.numeric(CCC_051)
     
     # verify for NA
-    if_else2(is.na(CCC_121), 0, CCC_121)
-    if_else2(is.na(CCC_131), 0, CCC_131)
-    if_else2(is.na(CCC_151), 0, CCC_151)
-    if_else2(is.na(CCC_171), 0, CCC_171)
-    if_else2(is.na(CCC_280), 0, CCC_280)
-    if_else2(is.na(resp_condition), 0, resp_condition)
-    if_else2(is.na(CCC_051), 0, CCC_051)
+    CCC_121 <- if_else2(is.na(CCC_121), 0, CCC_121)
+    CCC_131 <- if_else2(is.na(CCC_131), 0, CCC_131)
+    CCC_151 <- if_else2(is.na(CCC_151), 0, CCC_151)
+    CCC_171 <- if_else2(is.na(CCC_171), 0, CCC_171)
+    CCC_280 <- if_else2(is.na(CCC_280), 0, CCC_280)
+    resp_condition <- if_else2(is.na(resp_condition), 0, resp_condition)
+    CCC_051 <- if_else2(is.na(CCC_051), 0, CCC_051)
     
     # adjust resp_condition to yes = 1, no = 2
     resp_condition <- if_else2(resp_condition %in% c(1:2), 1,
@@ -27,5 +27,5 @@ multiple_conditions_fun <-
     conditions <- (CCC_121%%2) + (CCC_131%%2) + (CCC_151%%2) +(CCC_171%%2) +
       (CCC_280%%2) + (resp_condition%%2) + (CCC_051%%2)
     
-    ifelse(conditions>= 5, "5+", conditions)
+    if_else2(conditions>= 5, "5+", conditions)
   }
