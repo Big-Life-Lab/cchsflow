ff5f58a1e8a8f340538c53dfecc89762953a6c59
Warsame Yusuf <wyusu068@Uottawa.ca>
2020-02-06 11:42:34 -0500

    [Feature] add documentation for multiple_conditions_fun

--- R/multiple-conditions.R
+++ R/multiple-conditions.R
@@ -1,3 +1,54 @@
+#' @title Multiple chronic conditions
+#' 
+#' @description This function generates a derived variable (multiple_conditions)
+#'  that counts the number of chronic conditions a respondent has. This function
+#'  takes 6 CCHS variables (heart disease, cancer, stroke, bowel disorder, and
+#'  arthritis), and well one derived variable (respiratory condition) to count
+#'  the number of conditions a respondent has.
+#'  
+#' @param CCC_121 variable indicating if respondent has heart disease
+#' 
+#' @param CCC_131 variable indicating if respondent has active cancer
+#' 
+#' @param CCC_151 variable indicating if respondent suffers from the effects
+#'  of a stroke
+#' 
+#' @param CCC_171 variable indicating if respondent has a bowel disorder
+#' 
+#' @param CCC_280 variable indicating if respondent has a mood disorder. Note,
+#'  variable was not asked to respondents in the 2001 CCHS survey cycle
+#' 
+#' @param resp_condition derived variable indicating if respondent has a
+#'  respiratory condition. See \code{\link{resp_condition_fun1}} for
+#'  documentation on how variable was derived
+#'
+#' @param CCC_051 variable indicating if respondent has arthritis or
+#'  rheumatism
+#'
+#' @note mood disorder (CCC_280) was not asked to respondents in the 2001 CCHS
+#'  survey cycle. This mean respondents in this cycle will only be able to have
+#'  a maximum of 6 chronic conditions as opposed to 7 for respondents in other
+#'  cycles.
+#' 
+#' @return A categorical variable indicating the number of chronic conditions
+#'  a respondent has. Respondents with 5 or more conditions are grouped in the
+#'  "5+" category. 
+#' 
+#' @examples 
+#'  # Using multiple_conditions_fun() to generate multiple_conditions in a CCHS
+#'  # cycle.
+#'  
+#'  # multiple_conditions_fun() is specified in variable_details.csv along with
+#'  # the CCHS variables and cycles included.
+#'  
+#'  library(cchsflow)
+#'  conditions_2010 <- rec_with_table(cchs2010, c("CCC_121","CCC_131","CCC_151",
+#'  "CCC_171","CCC_280","resp_condition","CCC_051"))
+#'  
+#'  head(conditions_2010)
+#'  
+#' @export
+
 multiple_conditions_fun <- 
   function(CCC_121,CCC_131,CCC_151,CCC_171,CCC_280,resp_condition,CCC_051){
     
