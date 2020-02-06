e452099fe888f0bdadd21825a49ae0b58fb360d7
Warsame Yusuf <wyusu068@Uottawa.ca>
2020-02-06 15:02:33 -0500

    [Refactor] split multiple_conditions_fun into 2 functions to account to missing CCC_280 in 2001 cchs

--- R/multiple-conditions.R
+++ R/multiple-conditions.R
@@ -2,9 +2,98 @@
 #' 
 #' @description This function generates a derived variable (multiple_conditions)
 #'  that counts the number of chronic conditions a respondent has. This function
-#'  takes 6 CCHS variables (heart disease, cancer, stroke, bowel disorder, and
+#'  takes 5 CCHS variables (heart disease, cancer, stroke, bowel disorder, and
 #'  arthritis), and well one derived variable (respiratory condition) to count
 #'  the number of conditions a respondent has.
+#'  
+#' @param CCC_121 variable indicating if respondent has heart disease
+#' 
+#' @param CCC_131 variable indicating if respondent has active cancer
+#' 
+#' @param CCC_151 variable indicating if respondent suffers from the effects
+#'  of a stroke
+#' 
+#' @param CCC_171 variable indicating if respondent has a bowel disorder
+#' 
+#' @param resp_condition_der derived variable indicating if respondent has a
+#'  respiratory condition. See \code{\link{resp_condition_fun1}} for
+#'  documentation on how variable was derived
+#'
+#' @param CCC_051 variable indicating if respondent has arthritis or
+#'  rheumatism
+#'
+#' @details mood disorder (CCC_280) was not asked to respondents in the 2001
+#'  CCHS survey cycle. This mean respondents in this cycle will only be able to
+#'  have a maximum of 6 chronic conditions as opposed to 7 for respondents in
+#'  other cycles. \code{\link{multiple_conditions_fun2}} is used for CCHS cycles
+#'  from 2003 to 2014.
+#' 
+#' @return A categorical variable indicating the number of chronic conditions
+#'  a respondent has. Respondents with 5 or more conditions are grouped in the
+#'  "5+" category. 
+#' 
+#' @examples 
+#'  # Using rec_with_table() to generate multiple_conditions in a CCHS
+#'  # cycle.
+#'  
+#'  # multiple_conditions_fun1() is specified in variable_details.csv along with
+#'  # the CCHS variables and cycles included.
+#'  
+#'  # To generate multiple_conditions, use rec_with_table() and specify the
+#'  # multiple_conditions, along with the variables that are derived from it.
+#'  # Since resp_condition_der is also a derived variable, you will have to
+#'  # specify the variables that are derived from it. In this example, data
+#'  # from the 2001 CCHS will be used, so DHHGAGE_cont, CCC_091, and CCC_91A,
+#'  # and CCC_031 will be specified along with resp_condition_der.
+#'  
+#' library(cchsflow)
+#'  conditions_2001 <- rec_with_table(cchs2001, c("DHHGAGE_cont", "CCC_091",
+#'  "CCC_91A", "CCC_031", "CCC_121","CCC_131","CCC_151", "CCC_171","CCC_280",
+#'  "resp_condition_der","CCC_051", "multiple_conditions"))
+#'  
+#'  head(conditions_2001)
+#' 
+#' @seealso \code{\link{multiple_conditions_fun2}}     
+#' @export
+
+multiple_conditions_fun1 <- 
+  function(CCC_121,CCC_131,CCC_151,CCC_171,resp_condition_der,CCC_051){
+    
+    # Convert variables to numeric
+    CCC_121 <- as.numeric(CCC_121)
+    CCC_131 <- as.numeric(CCC_131)
+    CCC_151 <- as.numeric(CCC_151)
+    CCC_171 <- as.numeric(CCC_171)
+    resp_condition_der <- as.numeric(resp_condition_der)
+    CCC_051 <- as.numeric(CCC_051)
+    
+    # set invalid/NA values to 0
+    CCC_121 <- if_else2(CCC_121 %in% (1:2), CCC_121, 0)
+    CCC_131 <- if_else2(CCC_131 %in% (1:2), CCC_131, 0)
+    CCC_151 <- if_else2(CCC_151 %in% (1:2), CCC_151, 0)
+    CCC_171 <- if_else2(CCC_171 %in% (1:2), CCC_171, 0)
+    resp_condition_der <- if_else2(resp_condition_der %in% (1:3),
+                                   resp_condition_der, 0)
+    CCC_051 <- if_else2(CCC_051 %in% (1:2), CCC_051, 0)
+    
+    # adjust resp_condition to yes = 1, no = 2
+    resp_condition_der <- if_else2(resp_condition_der %in% c(1:2), 1, 2)
+    
+    # Calculate number of conditions based on yes
+    conditions <- 
+      (CCC_121%%2) + (CCC_131%%2) + (CCC_151%%2) +(CCC_171%%2) +
+      (resp_condition_der%%2) + (CCC_051%%2)
+    
+    if_else2(conditions>= 5, "5+", conditions)
+  }
+
+#' @title Multiple chronic conditions
+#' 
+#' @description This function generates a derived variable (multiple_conditions)
+#'  that counts the number of chronic conditions a respondent has. This function
+#'  takes 6 CCHS variables (heart disease, cancer, stroke, bowel disorder, mood
+#'  disorder and arthritis), and well one derived variable (respiratory
+#'  condition) to count the number of conditions a respondent has.
 #'  
 #' @param CCC_121 variable indicating if respondent has heart disease
 #' 
@@ -25,10 +114,11 @@
 #' @param CCC_051 variable indicating if respondent has arthritis or
 #'  rheumatism
 #'
-#' @note mood disorder (CCC_280) was not asked to respondents in the 2001 CCHS
-#'  survey cycle. This mean respondents in this cycle will only be able to have
-#'  a maximum of 6 chronic conditions as opposed to 7 for respondents in other
-#'  cycles.
+#' @details mood disorder (CCC_280) was not asked to respondents in the 2001
+#'  CCHS survey cycle. This mean respondents in this cycle will only be able to
+#'  have a maximum of 6 chronic conditions as opposed to 7 for respondents in
+#'  other cycles. \code{\link{multiple_conditions_fun1}} is used for CCHS cycles
+#'  from 2003 to 2014.
 #' 
 #' @return A categorical variable indicating the number of chronic conditions
 #'  a respondent has. Respondents with 5 or more conditions are grouped in the
@@ -54,10 +144,11 @@
 #'  "resp_condition_der","CCC_051", "multiple_conditions"))
 #'  
 #'  head(conditions_2010)
-#'  
+#' 
+#' @seealso \code{\link{multiple_conditions_fun1}} 
 #' @export
 
-multiple_conditions_fun <- 
+multiple_conditions_fun2 <- 
   function(CCC_121,CCC_131,CCC_151,CCC_171,CCC_280,resp_condition_der,CCC_051){
     
     # Convert variables to numeric
@@ -68,7 +159,7 @@ multiple_conditions_fun <-
     CCC_280 <- as.numeric(CCC_280)
     resp_condition_der <- as.numeric(resp_condition_der)
     CCC_051 <- as.numeric(CCC_051)
-    
+
     # set invalid/NA values to 0
     CCC_121 <- if_else2(CCC_121 %in% (1:2), CCC_121, 0)
     CCC_131 <- if_else2(CCC_131 %in% (1:2), CCC_131, 0)
