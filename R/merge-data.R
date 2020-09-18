15ff7289cd18b7f639567a59bf4db4c4d663413f
Warsame Yusuf <wyusu068@Uottawa.ca>
2020-09-18 16:23:49 -0400

    [Refactor] fix for loop

--- R/merge-data.R
+++ R/merge-data.R
@@ -1,26 +1,25 @@
-merge_data <- function(data, ...) {
+merge_data <- function(...) {
   # Step 1: bind datasets
-  new_data <- bind_rows(data, ...)
+  new_data <- bind_rows(...)
   
   # Step 2: apply NA(c) to untagged NA's
-  for (variable in new_data$columns) {
-    NA_index <- is.na(new_data[[variable]])
-    tagged_NA_index <- haven::is_tagged_na(new_data[[variable]])
+  for (i in names(new_data)) {
+    NA_index <- is.na(new_data[[i]])
+    tagged_NA_index <- haven::is_tagged_na(new_data[[i]])
     true_NA_index <- !(NA_index == tagged_NA_index)
-    if (is.numeric(new_data[[variable]])) {
-      new_data[true_NA_index, variable] <-
+    if (is.numeric(new_data[[i]])) {
+      new_data[true_NA_index, i] <-
         "NA(c)"
     } else {
-      if (!"NA(c)" %in% levels(new_data[[variable]])) {
-        levels(new_data[[variable]]) <-
-          c(levels(new_data[[variable]]),
+      if (!"NA(c)" %in% levels(new_data[[i]])) {
+        levels(new_data[[i]]) <-
+          c(levels(new_data[[i]]),
             "NA(c)")
       }
-      new_data[true_NA_index, variable] <-
+      new_data[true_NA_index, i] <-
         "NA(c)"
     }
   }
-  
   # Step 3: apply labels to combined dataset
   labelled_data <- set_data_labels(new_data, variable_details,
                                    variables)
