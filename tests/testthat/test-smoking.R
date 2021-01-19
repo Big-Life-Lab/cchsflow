165d8676bdfd2937d78d7cd2507d39b2d4f2dbca
Warsame Yusuf <wyusu068@Uottawa.ca>
2021-01-19 09:56:39 -0500

    [Feature] update unit testing and pkg description

--- tests/testthat/test-smoking.R
+++ tests/testthat/test-smoking.R
@@ -1,23 +1,17 @@
 test_that("pack_years_fun() has expected outputs when
           SMKDSTY is out of range", {
-  expect_equal(pack_years_fun(10, 40, 6, 6, 22, 96, 12, 996, 996, 96, 96, 1),
+  expect_equal(pack_years_fun(10, 40, 6, 6, 22, 96, 12, 996, 996, 96, 96),
                tagged_na("b"))
 })
 
 test_that("pack_years_fun() has expected outputs when
           DHHGAGE_cont is out of range", {
-  expect_equal(pack_years_fun(1, -1, 6, 6, 22, 96, 12, 996, 996, 96, 96, 1),
+  expect_equal(pack_years_fun(1, -1, 6, 6, 22, 96, 12, 996, 996, 96, 96),
                tagged_na("b"))
 })
 
 test_that("pack_years_fun() has expected outputs when
-          SMKG203_cont is out of range", {
-  expect_equal(pack_years_fun(1, 40, 6, 6, 100, 96, 12, 996, 996, 96, 96, 1),
-               0.0137)
-})
-
-test_that("pack_years_fun() has expected outputs when
           all parameters in range", {
-  expect_equal(pack_years_fun(1, 40, 6, 6, 22, 96, 12, 996, 996, 96, 96, 1),
-               10.8)
+  expect_equal(pack_years_fun(1, 40, 96, 16, 96, 10, 5, 996, 996, 96, 96),
+               12)
 })
