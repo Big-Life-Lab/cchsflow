95b7cf57d113df45dac3effb698f458d0eb51bc7
Kitty Chen <kittychenn27@gmail.com>
2021-09-30 10:04:59 -0400

    [Refactor] update smoking documentation notes

--- R/smoking.R
+++ R/smoking.R
@@ -318,17 +318,21 @@ pack_years_fun <-
 
 #' @title Age started smoking daily - daily/former daily smokers
 #'
-#' @description This function creates a continuous derived variable (SMKG040_fun)
-#' that calculates the approximate age that a daily or former daily smoker began 
-#' smoking daily. 
+#' @description This function creates a continuous derived variable 
+#' (SMKG040_fun) that calculates the approximate age that a daily or former 
+#' daily smoker began smoking daily. 
 #'
 #' @details SMKG203 (daily smoker) and SMKG207 (former daily) are present in
 #' CCHS 2001-2014, and are separate variables. For CCHS 2015 and onward, SMKG040 
-#' (daily/former daily) combines the two previous variables.As such, age started 
-#' smoking daily - daily/former daily smokers has a large number of missing 
-#' respondents for CCHS 2015 survey cycles and onward.SMKG040_fun takes the
-#' continuous functions (SMKG203_cont and SMKG207_cont) to create SMKG040 for 
-#' 2001-2014.
+#' (daily/former daily) combines the two previous variables. SMKG040_fun takes 
+#' the continuous functions (SMKG203_cont and SMKG207_cont) to create SMKG040 
+#' for 2001-2014.
+#' 
+#' @note In previous cycles, both SMKG203 and SMKG207 included respondents who 
+#' did not state their smoking status. From CCHS 2015 and onward, SMKG040 only
+#' included respondents who specified daily smoker or former daily smoker. As 
+#' a result, SMKG040 has a large number of missing respondents for CCHS 2015 
+#' survey cycles and onward.
 #'
 #' @param SMKG203_cont age started smoking daily. Variable asked to daily
 #'  smokers.
@@ -336,7 +340,7 @@ pack_years_fun <-
 #' @param SMKG207_cont age started smoking daily. Variable asked to former
 #'  daily smokers.
 #'  
-#' @example 
+#' @examples  
 #' Using SMKG040_fun() to create age values across CCHS cycles
 #' SMKG040_fun() is specified in variable_details.csv under SMKG040_cont.
 #' 
@@ -361,8 +365,8 @@ pack_years_fun <-
 #'
 #' tail(age_smoke_dfd_2011_2012)
 #'
-#' combined_age_smoke_dfd <- suppressWarnings(merge_rec_data(age_smoke_dfd_2009_2010,
-#'  age_smoke_dfd_2011_2012))
+#' combined_age_smoke_dfd <- suppressWarnings(merge_rec_data
+#' (age_smoke_dfd_2009_2010,age_smoke_dfd_2011_2012))
 #'
 #' head(combined_age_smoke_dfd)
 #' tail(combined_age_smoke_dfd)
